a2da7666ec3b209790fa3a2ed99564aa4ad5b671
RayRoyal1876 <tenthwonderind@icloud.com>
2020-11-09 15:37:14 -0500

    pass test

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class NewTest {
+    @Test
+    public void test1(){
+//        fail("not yet implemented");
+        assertEquals(5, 5);
+    }
+}
