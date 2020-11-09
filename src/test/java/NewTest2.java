3563b088bcfbe9b076573062103779f768ebf1bb
RayRoyal1876 <tenthwonderind@icloud.com>
2020-11-09 15:41:39 -0500

    failed test

---
+++ src/test/java/NewTest2.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class NewTest2 {
+    @Test
+    public void test(){
+        fail("not yet implemented");
+    }
+}
